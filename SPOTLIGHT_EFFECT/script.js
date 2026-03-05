0138e636b2b6b71edbec68cf104868423e6e78ff
sk <kumarshatrudhan94263@gmail.com>
2026-03-06 00:55:23 +0530

    add text animation

--- SPOTLIGHT_EFFECT/script.js
+++ SPOTLIGHT_EFFECT/script.js
@@ -2,3 +2,30 @@ addEventListener('mousemove',(e)=>{
 document.body.style.setProperty("--x",e.clientX+'px')
 document.body.style.setProperty("--y",e.clientY+'px')
 })
+
+
+
+const p = document.querySelector('p')
+ const text = p.innerText 
+
+const characters = "ABCDFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+let iteration = 0
+
+function randomText(){
+    const str =text.split("").map((char,index) => {
+        if(index<iteration){
+            return char
+        }
+        return characters.split("")[Math.floor(Math.random()*53)]
+
+    }).join("")
+    p.innerText = str
+    iteration += 0.25
+   
+}
+setInterval(randomText,50)
+
+
+
+
